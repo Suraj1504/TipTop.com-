1f37dea0c3b47393d881650f587b106cbf54d9f0
Suraj <surajsinghkaintura90@gmail.com>
2022-09-25 23:09:16 +0530

    login sigup completed

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function callLoginApi() {
-    event.preventDefault();
-    console.log("Sending data", email, password);
+  function callLoginApi(values) {
+    console.log("Sending data", values.email, values.password);
   }
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
+  const schema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().min(8).required(),
+  });
+
+  const { handleSubmit, values, handleChange, errors, handleBlur, touched } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      onSubmit: callLoginApi,
+      validationSchema: schema,
+    });
 
   return (
     <div className="h-screen px-12 py-6 bg-gray-200 ">
       <form
-        onSubmit={callLoginApi}
+        onSubmit={handleSubmit}
         className="flex flex-col h-full px-32 py-12 bg-white"
       >
         <h1 className="pb-4 font-sans text-3xl font-semibold text-gray-700">
@@ -34,38 +39,51 @@ function Login() {
             Username or email address
           </label>
           <input
-            value={email}
-            onChange={handleEmailChange}
+            value={values.email}
+            name="email"
+            type="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
             id="email-address"
             className="relative block w-full p-2 text-gray-900 border border-gray-400 appearance-none focus:outline-none"
           />
+          {touched.email && (
+            <div className="text-primary-default">{errors.email}</div>
+          )}
           <label
-            htmlFor="email-address"
+            htmlFor="password"
             className="mt-3 font-sans font-medium text-gray-700 text-md"
           >
             Password
           </label>
           <input
-            value={password}
-            onChange={handlePasswordChange}
-            id="email-address"
+            value={values.password}
+            name="password"
+            type="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            id="password"
             className="relative block w-full p-2 text-gray-900 border border-gray-400 appearance-none focus:outline-none"
           />
+          {touched.password && (
+            <div className="text-primary-default">{errors.password}</div>
+          )}
           <div className="flex gap-1 mt-3">
             <input type="checkbox" id="forRemember" name="forRemember" />
             <label for="forRemember" className="font-medium text-gray-700 ">
               Remember me
             </label>
           </div>
-          <button className="py-1 mt-1 text-lg font-bold text-white rounded-md px-7 bg-primary-default hover:bg-primary-dark w-fit">
+          <button
+            type="submit"
+            className="py-1 mt-1 text-lg font-bold text-white rounded-md px-7 bg-primary-default hover:bg-primary-dark w-fit"
+          >
             LOG IN
           </button>
           <a href="" className="mt-2 text-primary-default">
             Lost your password?
           </a>
         </div>
-
-        <div></div>
       </form>
     </div>
   );
